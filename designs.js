36ebbb08ed20536ad91977b3c5623b6a07a87983
Adrian Van Sickel <adrianvansickel@gmail.com>
2017-10-03 16:24:58 -0700

    initial commit

---
+++ designs.js
@@ -0,0 +1,15 @@
+// Select color input
+let type_color = $('#colorPicker').val();
+$('#colorPicker').change(function() {
+  type_color = $('#colorPicker').val();
+});
+
+// Select size input
+let grid_height = $('#input_height').val();
+let grid_width = $('#input_width').val();
+
+// When size is submitted by the user, call makeGrid()
+
+function makeGrid() {
+
+}
