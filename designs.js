c365b9afcdc3d3ad4ab5d1cef6c22c3d576a1c66
anvansic <adrianvansickel@gmail.com>
2017-10-04 00:06:23 -0700

    Preventing page refresh on form submit

--- designs.js
+++ designs.js
@@ -1,15 +1,32 @@
 // Select color input
-let type_color = $('#colorPicker').val();
-$('#colorPicker').change(function() {
-  type_color = $('#colorPicker').val();
-});
 
 // Select size input
-let grid_height = $('#input_height').val();
-let grid_width = $('#input_width').val();
+$('#sizePicker').submit(function(e) {
+  e.preventDefault();
+  makeGrid();
+});
 
 // When size is submitted by the user, call makeGrid()
-
 function makeGrid() {
+  let height = $('#input_height').val();
+  let width = $('#input_width').val();
 
+  $('#input_height').attr('value', height);
+  $('#input_width').attr('value', width);
+
+  $('#pixel_canvas').css('width', width*32);
+  $('#pixel_canvas').css('height', height*32);
+
+  //TODO Change this so that the grid actually shows up on the page.
+  for(var j=0; j<height; j++) {
+    $('#pixel_canvas').add('tr');
+    $('tr').addClass('row_'+j);
+    $('.row_'+j).css('border', '2px solid black');
+
+    for(var i=0; i<width; i++) {
+      $('.row_'+j).add('col');
+      $('col').addClass('column_'+i);
+      $('.column_'+i).css('border', '2px solid black');
+    }
+  }
 }
